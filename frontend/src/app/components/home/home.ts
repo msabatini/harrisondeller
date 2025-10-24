24d656055127f26f8b39698299298266783c0f9b
Michael Sabatini <michael@example.com>
2025-10-23 21:46:46 -0400

    fix: remove parallax effect from home page hero

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,25 +7,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class HomeComponent implements OnInit {
-  @ViewChild('hero') heroElement!: ElementRef;
-  parallaxOffset = 0;
+export class HomeComponent {
 
-  ngOnInit(): void {
-    this.updateParallax();
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll(): void {
-    this.updateParallax();
-  }
-
-  private updateParallax(): void {
-    const scrollPosition = window.scrollY;
-    this.parallaxOffset = scrollPosition * 0.5;
-
-    if (this.heroElement && this.heroElement.nativeElement) {
-      this.heroElement.nativeElement.style.backgroundPosition = `center ${-this.parallaxOffset}px`;
-    }
-  }
 }
